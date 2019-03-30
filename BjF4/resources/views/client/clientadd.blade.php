a82c6b453afda37f73a993308bd7c08e6819ce5d
lmh <461043365@qq.com>
2019-03-30 13:41:25 +0800

    改变

--- BjF4/resources/views/client/clientadd.blade.php
+++ BjF4/resources/views/client/clientadd.blade.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: LiuMH
- * Date: 2019/3/30
- * Time: 10:50
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>客户管理添加</title>
+</head>
+<body>
+    
+</body>
+</html>
